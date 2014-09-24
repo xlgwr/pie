b2c9ccd5f41418c66e347a700c886e9f618846c2
xlgwr <xlgwr@163.com>
2014-09-24 08:28:37 +0800

    add lib

---
+++ IDR.WCF/test.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IDR.WCF
+{
+    public partial class test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Service1 s = new Service1();
+            var plr = s.GetPlrMstrList("R1409220012", 2);
+            Response.Write(plr);
+        }
+    }
+}
