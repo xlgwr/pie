f86041e66304d3f0fafde4938439cade4ea1d49b
xlgwr <xlgwr@163.com>
2014-12-15 17:06:56 +0800

    init idr.mvc

---
+++ IDR.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IDR.MVC.Startup))]
+namespace IDR.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
